6c9e16806d32b13e084fac2b466f995e3f07372e
meiisawbsn <meiisawibisana@gmail.com>
2024-02-01 12:18:40 +0800

    update app

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ if selected == "Clustering":
 if selected == "About":
     st.title(f"It'Me")
 
-upFile = st.file_uploader("Choose a File")
-
-dataframe = pd.read_csv(upFile)
-st.write(dataframe)
+upFile = st.file_uploader("Choose a File", type=["csv"])
+if upFile is not None:
+    dataframe = pd.read_csv(upFile)
+    st.write(dataframe)
 
 # Create a selectbox for column selection
 selected_column = st.multiselect('Select a column', dataframe.columns)
