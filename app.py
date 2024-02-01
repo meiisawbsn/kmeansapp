f1e830eeb8bd7080a81de3e86ffaae88949b5108
meiisawbsn <meiisawibisana@gmail.com>
2024-02-01 14:24:19 +0800

    update app

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_score,davies_bouldin_score
 
 st.title("K-MEANS")
+st.title("Yang belum : ")
+st.title("- Euclidean Distance")
+st.title("- Pembobotan")
 
 def plot_elbow_method(selected_data):
     wcss = []
@@ -19,6 +23,30 @@ def plot_elbow_method(selected_data):
     plt.xlabel('Number of Clusters')
     plt.ylabel('WCSS')  # Within cluster sum of squares
     return plt
+
+def perform_clustering(selected_data, n_clusters, iteration):
+    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', random_state=None, n_init=iteration)
+    kmeans.fit(selected_data)
+    selected_data['Cluster'] = kmeans.labels_
+    return selected_data
+
+# def calculate_euclidean_score(selected_data, cluster_centers):
+#     euclidean_scores = []
+#     for i, data_point in selected_data.iterrows():
+#         cluster_label = data_point['Cluster']
+#         centroid = cluster_centers[cluster_label]
+#         euclidean_distance = np.linalg.norm(data_point[selected_columns] - centroid)  # Euclidean distance formula
+#         euclidean_scores.append(euclidean_distance)
+#     return euclidean_scores
+
+def map_cluster_labels(df):
+    cluster_mapping = {1: 'Layak', 0: 'Tidak Layak'}
+    df['Cluster_Text'] = df['Cluster'].map(cluster_mapping)
+    return df
+
+def calculate_dbi(selected_data, labels):
+    dbi_score = davies_bouldin_score(selected_data, labels)
+    return dbi_score
 
 with st.sidebar:
     selected = option_menu("MENU", ["Home",'Clustering','About'], 
@@ -45,8 +73,30 @@ if upFile is not None:
         
         fig = plot_elbow_method(selected_data)
         st.pyplot(fig)
-        
+            
+        # k = st.slider('Select number of clusters', min_value=2, max_value=10)
+        k=2
+        # Specify number of iterations
+        num_iterations = st.slider('Select number of iterations', min_value=1, max_value=10)
+
+        for i in range(1, num_iterations + 1):
+            clustered_data  = perform_clustering(selected_data, k, i)
+        for i in range(1, num_iterations + 1):
+            cluster_centers  = perform_clustering(selected_data, k, i)
+
         kmeans = KMeans(n_clusters = 2, init = 'k-means++', random_state = None)
         y_kmeans = kmeans.fit_predict(selected_data)
         silhouette_score_average = silhouette_score(selected_data, y_kmeans)
-        st.write("silhouette Score 2 kluster =", silhouette_score_average)
+        st.write(f"Silhouette Score = {silhouette_score_average}")
+
+        dbi_score = calculate_dbi(selected_data, clustered_data['Cluster'])
+        st.write(f"Davies-Bouldin Index = {dbi_score}")
+
+        # euclidean_scores = calculate_euclidean_score(clustered_data, cluster_centers)
+        # st.write("Euclidean Distance Scores:")
+        # st.write(euclidean_scores)
+      
+        # Visualize results
+        st.title("Clustering Result")
+        clustered_data = map_cluster_labels(clustered_data)
+        st.write(clustered_data)
